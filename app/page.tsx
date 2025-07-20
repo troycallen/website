3145c8166400e2e338a1efaa9db5d55247bc4955
troycallen <165961938+troycallen@users.noreply.github.com>
2025-07-19 21:45:53 -0400

    make the font bigger

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,9 @@ export default function Home() {
           </div>
 
           <div className="flex-1">
-            <h1 className="text-5xl font-bold mb-2">Troy Allen</h1>
-            <p className="text-xl text-gray-400 mb-4">CS and ML @ Georgia Tech</p>
-            <p className="text-gray-300 mb-6">
+            <h1 className="text-6xl font-bold mb-2">Troy Allen</h1>
+            <p className="text-2xl text-gray-500 mb-4">CS & ML @ Georgia Tech</p>
+            <p className="text-lg text-gray-400 mb-6">
               Avid builder and tinkerer. Hoping to have an impact on the world using technology.
             </p>
 
